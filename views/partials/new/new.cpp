3e41aed72170b14d7539a135eba2bb5f113a57ad
Rounit Singh <rounitksingh@gmail.com>
2022-09-09 11:29:43 +0530

    Chenges to css

---
+++ views/partials/new/new.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   string a;
+   string b;
+   cin>>a>>b;
+   string c = a + b;
+   cout<< a << " "<<b<<endl;
+   cout<< c;
+   char c[0] = a[0];
+    return 0;
+}
